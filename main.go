832d1a2dd58c827194383f9cb347f67ca0c9f332
amit bhagat <amitbhgat2506@gmail.com>
2023-02-24 17:22:02 +0530

    basic golang response write hello world code for web dev

--- main.go
+++ main.go
@@ -1,19 +1,52 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		n, err := fmt.Fprintf(writer, "hello world")
-		if err != nil {
-			fmt.Println("err")
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
+const portNumber = ":8080"
 
-	_ = http.ListenAndServe(":8080", nil)
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 3)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the home page 2 + 3 is %d", sum))
+}
+
+func addValues(x, y int) int {
+	return x + y
+}
+
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(0, 0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divivde by 0")
+		return
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%f divide by %f is %f", 100.0, 10.0, f))
+}
+
+func divideValues(x float32, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
+func main() {
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/About", About)
+	http.HandleFunc("/Divide", Divide)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 
 }
